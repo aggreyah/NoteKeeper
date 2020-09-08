ea11d070eaa0f6adb71d8762998be8ed72de3e21
aggreyah <aggreyah@gmail.com>
2020-09-08 14:24:50 +0300

    still trying to get the creation of database right.

--- app/src/main/java/com/aggreyah/notekeeper/NoteKeeperOpenHelper.java
+++ app/src/main/java/com/aggreyah/notekeeper/NoteKeeperOpenHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
-    public static  final String DATABASE_NAME = "note_keepr.db";
+    public static  final String DATABASE_NAME = "NoteKeeper.db";
     public static final int DATABASE_VERSION = 1;
     public NoteKeeperOpenHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
